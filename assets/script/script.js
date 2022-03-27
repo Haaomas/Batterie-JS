9da080362a45a85b9805f44979a716f66d07a142
Haaomas <barreauji@gmail.com>
2022-03-27 18:40:40 +0200

    Fonction V1
    Fonction de batterie opérationnel
    Possibilité d'amélioration

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,2 +1,22 @@
+//TEST de connexion
 console.log("Be water my Friend");
 
+//Début du projet 
+document.addEventListener("keydown", function (e) {
+    //Récupère la key de la touche du clavier  
+    let keyClick = e.keyCode; //TODO Trouver une version mise à jour 
+
+    //Sélection dynamique des div pour toggle de classe
+    let testDiv = document.querySelector(`div[data-key="${keyClick}"]`); //TODO Modifier le Toggle 
+    testDiv.classList.toggle("addClass");
+
+    //Sélection dynamique des fichiers audio
+    let audioSelect = document.querySelector(`audio[data-key="${keyClick}"]`);
+
+    //Création dynamique du fichier audio 
+    let audioCreate = new Audio(audioSelect.src);
+
+    // Lecture du fichier 
+    audioCreate.currentTime = 0;
+    audioCreate.play();
+});
