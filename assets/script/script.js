6e90e131c27b2fa9f8d335c0c644bd16866c1e31
Haaomas <barreauji@gmail.com>
2022-03-29 10:49:57 +0200

    Peaufinage terminé
    Amélioration Graphique
    Rajout d'une fonction de retrait de classe

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,24 +1,32 @@
-console.log("Be water my Friend");
-
 //TEST de connexion
 console.log("Be water my Friend");
 
 //Début du projet 
 document.addEventListener("keydown", function (e) {
-    //Récupère la key de la touche du clavier  
-    let keyClick = e.keyCode; //TODO Trouver une version mise à jour 
+    //Récupère la key de la touche du clavier, utilisation de key au lieu de keyCode qui est obsolète  
+    const keyClick = e.key;
+    console.log('keyClick', keyClick);
 
     //Sélection dynamique des div pour toggle de classe
-    let testDiv = document.querySelector(`div[data-key="${keyClick}"]`); //TODO Modifier le Toggle 
+    const testDiv = document.querySelector(`div[data-key="${keyClick}"]`);
     testDiv.classList.toggle("addClass");
 
     //Sélection dynamique des fichiers audio
-    let audioSelect = document.querySelector(`audio[data-key="${keyClick}"]`);
+    const audioSelect = document.querySelector(`audio[data-key="${keyClick}"]`);
+    //Si pas de fichier audio fin de la fonction 
+    if (!audioSelect) return;
 
     //Création dynamique du fichier audio 
-    let audioCreate = new Audio(audioSelect.src);
+    const audioCreate = new Audio(audioSelect.src);
+
 
     // Lecture du fichier 
     audioCreate.currentTime = 0;
     audioCreate.play();
+
+    //Retrait de la classe à la fin de la transition
+    document.addEventListener('transitionend', () => {
+        console.log('Transition terminée');
+        testDiv.classList.remove("addClass");
+    });
 });
